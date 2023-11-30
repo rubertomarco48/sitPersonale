6fe0748fe92fea3851b569a5e253b1cb08b5b097
Ruberto Marco <rubertomarco48@gmail.com>
2023-11-30 16:57:08 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import CookiePolicy from "./CookiePolicy";
 
 
 
+
 function App() {
   return (
     <BrowserRouter>
--- src/Cookie.jsx
+++ src/Cookie.jsx
@@ -1,6 +1,7 @@
 
 
 import { useCookies } from 'react-cookie';
+import { Link } from 'react-router-dom';
 
 const CookieBanner = () => {
   const [cookies, setCookie] = useCookies(['cookieConsent']);
@@ -13,7 +14,7 @@ const CookieBanner = () => {
     <div id='cook'>
       {!cookies.cookieConsent && (
         <div className=' gap-4 w-4/5 bg-white rounded-xl flex justify-center items-center'>
-          <p>Questo sito utilizza i cookie. Leggi la nostra <a href="/cookie-policy" className='policy'>Cookie Policy</a>.</p>
+          <p>Questo sito utilizza i cookie. Leggi la nostra <Link to="/cookie-policy" className='policy'>Cookie Policy</Link>.</p>
           <button className=' bg-green-600 p-4 text-slate-100 rounded-xl bottoneCookie' onClick={handleAccept}>Accetta i cookie</button>
         </div>
       )}
