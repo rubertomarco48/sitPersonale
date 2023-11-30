f5c7059b41c3173b475b3951819bf929cd023e5e
Ruberto Marco <rubertomarco48@gmail.com>
2023-11-30 17:09:58 +0100

    fix

--- src/CookiePolicy.jsx
+++ src/CookiePolicy.jsx
@@ -1,6 +1,6 @@
 const CookiePolicy = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center gap-4">
       <p className="w-4/5 text-xl">
         Privacy & Cookie Policy per il Sito Portfolio 
         Ultimo aggiornamento: 30/11/2023 
@@ -8,38 +8,44 @@ const CookiePolicy = () => {
         La tua privacy è importante per noi, e desideriamo informarti su come raccogliamo,
         utilizziamo e proteggiamo le informazioni personali che potremmo
         raccogliere attraverso il tuo utilizzo del Sito. 
-        
+        </p>
+        <p className="w-4/5 text-xl">
         1. Raccolta delle informazioni 
         Il Sito non raccoglie attivamente alcuna informazione
         personale. Non utilizziamo moduli di contatto, commenti o altri mezzi
         per acquisire dati personali dai visitatori del sito. 
-        
+        </p>
+        <p className="w-4/5 text-xl">
         2. Cookie 
         Il Sito potrebbe utilizzare cookie per migliorare l &rsquo; esperienza di navigazione,
         ma solo quelli strettamente necessari per il funzionamento del sito.
         Questi cookie non raccolgono informazioni personali e vengono
         automaticamente eliminati quando chiudi il browser. 
-        
+        </p>
+        <p className="w-4/5 text-xl">
         3. Condivisione di informazioni 
         Poiché non raccogliamo attivamente informazioni personali,
         non condividiamo alcuna informazione con terze parti. 
-        
+        </p>
+        <p className="w-4/5 text-xl">
         4. Sicurezza 
         Il Sito si impegna a proteggere le informazioni personali dei visitatori.
         Tuttavia, data la natura di un sito portfolio senza raccolta di dati
         attiva, non vi è alcuna informazione personale da proteggere. 
-        
+        </p>
+        <p className="w-4/5 text-xl">
         5.Modifiche alla Privacy & Cookie Policy 
         Ci riserviamo il diritto di apportare modifiche a questa Privacy & 
         Cookie Policy in qualsiasi momento. 
         Ogni modifica sarà effettiva non appena pubblicata sul Sito.
         L&rsquo;uso continuato del Sito dopo tali modifiche costituirà l&rsquo;accettazione
         delle stesse. 
-        
+        </p>
+        <p className="w-4/5 text-xl">
         Contattaci 
         Se hai domande o preoccupazioni sulla nostra
-        Privacy & Cookie Policy, non esitare a contattarci all&rsquo;indirizzo
-        rubertomarco48@gmail.com. 
+        Privacy & Cookie Policy, non esitare a contattarci all&rsquo;indirizzo 
+        <a href="mailto:rubertomarco48@gmail.com" className=" text-red-600 hover:text-violet-600"> rubertomarco48@gmail.com </a>. 
         Grazie per la tua fiducia nel visitare il nostro Sito.
       </p>
     </div>
